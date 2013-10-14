749bbdae57aae0d50d7930bd5afbb62bccd43ea6
kevin.heyvaert <kevin.heyvaert@student.artesis.be>
2013-10-14 19:26:16 +0200

    Classe GameStatus toegevoegd aan beide prjecten

---
+++ 2. game/MMSystems5Game/MMSystems5Game/Gamestate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MMSystems5Game
+{
+    public class Gamestate
+    {
+        List<Player> Players;
+        Player ActivePlayer;
+        Bord Mainbord;
+        GameFase CurrentFase;
+
+    }
+
+    enum GameFase { Buying, Walking, Bargaining }  //Aanpassen
+}
---
+++ 2. game/MMSystems5Silverlight/MMSystems5Silverlight.Web/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MMSystems5Silverlight.Web
+{
+    public class Gamestate
+    {
+        List<Player> Players;
+        Player ActivePlayer;
+        Bord Mainbord;
+        GameFase CurrentFase;
+
+    }
+
+    enum GameFase { Buying, Walking, Bargaining }  //Aanpassen
+}
