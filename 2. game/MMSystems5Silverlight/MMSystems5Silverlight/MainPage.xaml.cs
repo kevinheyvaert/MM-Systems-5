f9ed2969215558444d2af5d1f38436b16c75e051
kevin.heyvaert <kevin.heyvaert@student.artesis.be>
2013-10-14 17:54:07 +0200

    Service naar Azure gepusht in Silverlight (wou service van Azure gebruiken in silverlight maar gaf cross platform errors)

--- 2. game/MMSystems5Silverlight/MMSystems5Silverlight/MainPage.xaml.cs
+++ 2. game/MMSystems5Silverlight/MMSystems5Silverlight/MainPage.xaml.cs
@@ -15,6 +15,7 @@ namespace MMSystems5Silverlight
     public partial class MainPage : UserControl
     {
         ServiceReference1.GanzenbordServiceClient client;
+        //GanzenBordServiceAzure.GanzenbordServiceClient client1;
         
         Bord Speelbord;
         Player Speler;
@@ -24,11 +25,24 @@ namespace MMSystems5Silverlight
             Speelbord = new Bord();
             Speler = new Player();
             this.DataContext = Speler;
-           client = new ServiceReference1.GanzenbordServiceClient();
-           client.GooiCompleted += client_GooiCompleted;
+            client = new ServiceReference1.GanzenbordServiceClient();
+            client.GooiCompleted += client_GooiCompleted;
+
+           //client1 = new GanzenBordServiceAzure.GanzenbordServiceClient();
+           //client1.GooiCompleted += client1_GooiCompleted;
         
           
         }
+
+        //void client1_GooiCompleted(object sender, GanzenBordServiceAzure.GooiCompletedEventArgs e)
+        //{
+        //    //throw new NotImplementedException();
+        //    AantalDobbelsteen.Text = e.Result.ToString();
+        //    Speler.Locatie = Speler.Locatie + e.Result;
+        //    PlaatsOpBord.Text = Speler.Locatie.ToString();
+        //    Speler.PlaatsC = Speelbord.Plaats[Speler.Locatie, 0];
+        //    Speler.PlaatsR = Speelbord.Plaats[Speler.Locatie, 1];
+        //}
 
         void client_GooiCompleted(object sender, ServiceReference1.GooiCompletedEventArgs e)
         {
@@ -48,20 +62,9 @@ namespace MMSystems5Silverlight
         {
 
             client.GooiAsync();
+            //client1.GooiAsync();
 
-            //throw new NotImplementedException();
-            //client.GooiAsync();
-            //PlaatsOpBord.Text = Dobbelsteen1.Waarde.ToString();
-            //client.GooiAsync();
-           
-            //Dobbelsteen Dobbelsteen1 = new Dobbelsteen();
-            //Dobbelsteen1.GeefWaardeDobbelsteen();
-            //// Moet dit hier staan ? 
-            //Speler.Locatie = Dobbelsteen1.Waarde + Speler.Locatie;
-            //PlaatsOpBord.Text = Dobbelsteen1.Waarde.ToString();
-            //AantalDobbelsteen.Text = Speler.Locatie.ToString();
-            //Speler.PlaatsC = Speelbord.Plaats[Speler.Locatie, 0];
-            //Speler.PlaatsR = Speelbord.Plaats[Speler.Locatie, 1];
+            
 
         }
     }
